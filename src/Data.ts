57d42a5ee03e9a3a4ae019e0b215d0a442d846d4
Nikolay Ivanov <nikolay9619962@mail.ru>
2025-01-13 11:50:34 +0100

    Ghalette

--- src/Data.ts
+++ src/Data.ts
@@ -71,7 +71,7 @@ export const students: Record<Language, StudentAny>[] = [
         ru: {
             nickname: 'galetha',
             date: '',
-            title: '',
+            title: 'Потенциальный отказ в стажировке',
             briefProblem: 'Автоавария в которой получил такие травмы, как: перелом шейного отдела позвоночника, травма плеча, реабилитация',
             detailedProblem: 'У меня сложная ситуация, из-за которой я не успел подать документы на визу и из-за которой я не успеваю приехать в Париж до 10 января. 27 марта 2024 года я попал в автокатастрофу, в которой получил перелом шейного отдела позвоночника, множество ссадин и травму правого плеча. Готов предоставить документы, фотографии и заключения врачей. До конца этого года я проходил реабилитацию, из-за этого я не смог физически собрать необходимый перечень документов и подать документы на визу. Из-за этого прошу вас рассмотреть мою просьбу о переносе алюмнизации на 2 месяца, чтобы я успел сделать документы. Весь этот год, пока я проходил  реабилитацию, я все это время скучал по Ecole, именно учеба в Ecole давала мне силы каждый раз вставать и идти дальше.',
             request: 'Отменить алюмнизацию soldatovandrey4@gmail.com'
@@ -79,10 +79,10 @@ export const students: Record<Language, StudentAny>[] = [
         fr: {
             nickname: 'galetha',
             date: '',
-            title: '',
+            title: 'Refus potentiel de stage',
             briefProblem: 'Accident de voiture dans lequel il a subi des blessures telles que : fracture de la colonne cervicale, blessure à l\'épaule, rééducation.',
-            detailedProblem: 'J\'ai une situation difficile qui m\'a empêché de demander un visa et qui m\'empêchera de me rendre à Paris avant le 10 janvier. Le 27 mars 2024, j\'ai été victime d\'un accident de voiture au cours duquel j\'ai subi une fracture de la colonne cervicale, de multiples écorchures et une blessure à l\'épaule droite. Je suis prêt à fournir des documents, des photos et des rapports de médecins. Jusqu\'à la fin de cette année, j\'étais en rééducation, ce qui m\'a empêché de rassembler physiquement la liste des documents nécessaires et de demander un visa. Pour cette raison, je vous demande de prendre en considération ma demande de report de l\'alumnisation de 2 mois afin que j\'aie le temps de faire les documents. Pendant toute cette année, alors que j\'étais en rééducation, l\'Ecole m\'a manqué, ce sont mes études à l\'Ecole qui m\'ont donné la force de me relever et d\'aller de l\'avant à chaque fois. Traduit avec DeepL.com (version gratuite)',
-            request: 'Annuler l\'alumni soldatovandrey4@gmail.com'
+            detailedProblem: 'J\'ai une situation difficile qui m\'a empêché de demander un visa et qui m\'empêchera de me rendre à Paris avant le 10 janvier. Le 27 mars 2024, j\'ai été victime d\'un accident de voiture au cours duquel j\'ai subi une fracture de la colonne cervicale, de multiples écorchures et une blessure à l\'épaule droite. Je suis prêt à fournir des documents, des photos et des rapports de médecins. Jusqu\'à la fin de cette année, j\'étais en rééducation, ce qui m\'a empêché de rassembler physiquement la liste des documents nécessaires et de demander un visa. Pour cette raison, je vous demande de prendre en considération ma demande de report de l\'alumnisation de 2 mois afin que j\'aie le temps de faire les documents. Pendant toute cette année, alors que j\'étais en rééducation, l\'Ecole m\'a manqué, ce sont mes études à l\'Ecole qui m\'ont donné la force de me relever et d\'aller de l\'avant à chaque fois.',
+            request: 'Prévoir du temps pour arriver sur le campus avant le 20 avril 2025 soldatovandrey4@gmail.com'
         }
     },
 
@@ -101,7 +101,7 @@ export const students: Record<Language, StudentAny>[] = [
             title: 'Алюмнизация',
             briefProblem: '10 января - последний день до алюмнизации. Он должен был быть включительно. ',
             detailedProblem: 'я был убеждён, что \'0 дней before alumnization\' это менее 24 часов. То есть это означает, что после \'1 день до alumnization\' следует \'0 дней 23 часа 59 минут 59 секунд\', что вполне логично. после праздника я уже закончила выполнять проект ft_quine и пришла в школу для того, чтобы защитить его, но я стала alamni. прошу Вас отменить мою alamnization (сейчас я защищаю проект quine и могу его сдать прям сейчас). прошу Вас понять мою ситуацию. мы приехали в кампус и сделали визу специально, чтобы избежать alumnization',
-            request: 'Отменить алюмнизацию'
+            request: 'Запрос: Предоставьте время, чтобы прибыть в кампус до апреля 2025 года soldatovandrey4@gmail.com'
         }
     },
 
@@ -109,7 +109,7 @@ export const students: Record<Language, StudentAny>[] = [
         ru: {
             nickname: 'wjonatho',
             date: '',
-            title: '',
+            title: 'Потенциальный отказ в стажировке',
             briefProblem: '42 завалидировали мне стажировку, она заканчивается 17.03.2025. Правило, что я должен приехать до 10.01 появилось слишком поздно. Визу не получилось оформить так быстро ',
             detailedProblem: 'Если начать с начала, то начавшаяся война сильно ударила по бизнесу, который практически перестал стартовать стажировки, а если и стажировки были, то junior позиций не было. Людей увольняли после стажировок. Работу в такой ситуации было найти очень сложно, но все это время я думал о том, чтобы найти работу, завалидировать стажу. Вот осенью 2024 у меня получилось начать, я почитал все правила, понял что ничто не противоречит начать, а потом приехать в Париж сдавать пир видео. Тем более той внушительной суммы которую нужно показать консульству Франции у меня не было. И вот я спланировал, что в апреле переезжаю в Париж,   сдаю пирвидео, продолжаю обучение. А тут в декабре адм озвучивает в письме, что через 1 месяц (10.01.2025) алумнизируют меня и остальных, кто не логинился в кампусе. За 1 месяц, да еще и в новогодние праздники, там сумма огромная, чтобы так попасть в Париж. У меня уже была одна(30.12.2024) оценка стажировки от ментора и она прошла хорошо. Я все еще надеюсь, что я приеду в течение недели после второй оценки руководителя (17.03.2025) и сдам пирвидео, и продолжу свой путь в 42, достигну 17 lvl и получу квалификацию.',
             request: ''
@@ -117,9 +117,9 @@ export const students: Record<Language, StudentAny>[] = [
         fr: {
             nickname: 'wjonatho',
             date: '',
-            title: '',
+            title: 'Refus potentiel de stage',
             briefProblem: '42 J\'ai obtenu un stage qui se termine le 17.03.2025. La règle selon laquelle je dois arriver avant le 10.01.01 est apparue trop tard. Je n\'ai pas pu obtenir un visa aussi rapidement',
-            detailedProblem: 'Si vous commencez par le début, la guerre qui a éclaté a durement touché l\'entreprise, qui a pratiquement cessé de proposer des stages, et même s\'il y avait des stages, il n\'y avait pas de postes subalternes. Les gens étaient licenciés après leur stage. Il était très difficile de trouver un emploi dans cette situation, mais pendant tout ce temps, je pensais à trouver un emploi, à obtenir un stage. A l\'automne 2024, j\'ai réussi à commencer, j\'ai lu toutes les règles, j\'ai compris que rien ne s\'opposait à commencer, et puis je suis venu à Paris pour passer la vidéo de piratage. De plus, je n\'avais pas cette somme impressionnante d\'argent à présenter au consulat français. Et donc j\'ai prévu qu\'en avril je déménage à Paris, passe la pirvidéo, continue mes études. Et puis en décembre l\'administration fait savoir dans une lettre que dans 1 mois (10.01.2025) elle m\'alumnise ainsi que d\'autres qui ne se sont pas connectés au campus. En 1 mois, et même pendant les fêtes de fin d\'année, il y a une grosse somme d\'argent à débourser pour se rendre à Paris comme ça. J\'ai déjà eu une (30.12.2024) évaluation de stage par un mentor et ça s\'est bien passé. J\'espère toujours arriver dans la semaine qui suit ma deuxième évaluation par un superviseur (17.03.2025) et passer la pirvidéo et continuer mon voyage jusqu\'à 42, atteindre le niveau 17 et me qualifier. Traduit avec DeepL.com (version gratuite)',
+            detailedProblem: 'Si vous commencez par le début, la guerre qui a éclaté a durement touché l\'entreprise, qui a pratiquement cessé de proposer des stages, et même s\'il y avait des stages, il n\'y avait pas de postes subalternes. Les gens étaient licenciés après leur stage. Il était très difficile de trouver un emploi dans cette situation, mais pendant tout ce temps, je pensais à trouver un emploi, à obtenir un stage. A l\'automne 2024, j\'ai réussi à commencer, j\'ai lu toutes les règles, j\'ai compris que rien ne s\'opposait à commencer, et puis je suis venu à Paris pour passer la vidéo de piratage. De plus, je n\'avais pas cette somme impressionnante d\'argent à présenter au consulat français. Et donc j\'ai prévu qu\'en avril je déménage à Paris, passe la pirvidéo, continue mes études. Et puis en décembre l\'administration fait savoir dans une lettre que dans 1 mois (10.01.2025) elle m\'alumnise ainsi que d\'autres qui ne se sont pas connectés au campus. En 1 mois, et même pendant les fêtes de fin d\'année, il y a une grosse somme d\'argent à débourser pour se rendre à Paris comme ça. J\'ai déjà eu une (30.12.2024) évaluation de stage par un mentor et ça s\'est bien passé. J\'espère toujours arriver dans la semaine qui suit ma deuxième évaluation par un superviseur (17.03.2025) et passer la pirvidéo et continuer mon voyage jusqu\'à 42, atteindre le niveau 17 et me qualifier.',
             request: ''
         }
     },
